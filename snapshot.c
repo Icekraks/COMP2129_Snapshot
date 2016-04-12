0b5fd931f90c499c0a6164a3a3eff72ffe211163
Icekraks <stjimmy149@live.com.au>
2016-04-12 16:52:03 +1000

    fixed input bug

--- snapshot.c
+++ snapshot.c
@@ -1,7 +1,7 @@
 /**
  * comp2129 - assignment 2
- * <Felix Hu>
- * <fehu4705>
+ * <your name>
+ * <your unikey>
  */
 
 #include <stdio.h>
@@ -31,35 +31,36 @@ snapshot *snapshot_tail = NULL;
 // entry_delete
 // ...
 //
-const char *commands[] = {
+const char *commands[26] = {
         "BYE", //0
-        "HELP", //1
-        "LIST KEYS", //2
-        "LIST ENTRIES", //3
-        "LIST SNAPSHOTS", //4
-        "GET", //5
-        "DEL", //6
-        "PURGE", //7
-        "SET", //8
-        "PUSH", //9
-        "APPEND", //10
-        "PICK", //11
-        "PLUCK", //12
-        "POP", //13
-        "DROP", //14
-        "ROLLBACK", //15
-        "CHECKOUT", //16
-        "SNAPSHOT", //17
-        "MIN", //18
-        "MAX", //19
-        "SUM", //20
-        "LEN", //21
-        "REV", //22
-        "UNIQ", //23
-        "SORT", //24
-        "DIFF", //25
-        "INTER", //26
-        "UNION" //27
+        "HELP", //
+        "GET", //2
+        "DEL", //3
+        "PURGE", //4
+        "SET", //5
+        "PUSH", //6
+        "APPEND", //7
+        "PICK", //8
+        "PLUCK", //9
+        "POP", //10
+        "DROP", //11
+        "ROLLBACK", //12
+        "CHECKOUT", //13
+        "SNAPSHOT", //14
+        "MIN", //15
+        "MAX", //16
+        "SUM", //17
+        "LEN", //18
+        "REV", //19
+        "UNIQ", //20
+        "SORT", //21
+        "DIFF", //22
+        "INTER", //23
+        "UNION", //24
+        "LIST" //25
+//        "LIST KEYS", //26
+//        "LIST ENTRIES", //27
+//        "LIST SNAPSHOTS", //28
 
 };
 
@@ -71,11 +72,13 @@ void command_help() {
     printf("%s\n", HELP);
 }
 
-void command_list(snapshot *current_version, int cmdcounter, char *cmd, int index_two) {
+void command_list(snapshot *current_version, int cmd_counter, char *cmd, int index_two) {
+    if (index_two == 1) {
 
+    }
 }
 
-void command_get() {
+void command_get(snapshot *current_version, int cmd_counter, char *cmd) {
 
 }
 
@@ -154,6 +157,7 @@ void command_uniq() {
 }
 
 void command_sort() {
+    //use qSort()
     printf("ok");
 }
 
@@ -169,13 +173,16 @@ void command_union() {
 
 }
 
-void command_compare(snapshot *current_version, int cmdcounter, char *cmd) {
+void command_compare(snapshot *current_version, int cmd_counter, char **cmd) {
     int i = 0;
     int index_one = 0;
+    int index_two = 0;
     bool isFound = false;
-    while (i < sizeof(commands)) {
-
-        if (strcasecmp(cmd[0], commands[i]) == true) {
+    bool isFound2 = false;
+    int size = (sizeof(commands) / sizeof(commands[0]));
+    while (i < size) {
+        int result = strcasecmp(cmd[0], commands[i]);
+        if (result == 0) {
             isFound = true;
             index_one = i;
             break;
@@ -186,83 +193,77 @@ void command_compare(snapshot *current_version, int cmdcounter, char *cmd) {
         return;
         //incorrect command
     }
-    if (index_one==2||index_one==3||index_one==4) {
+    if (cmd[1] != NULL && index_one == 25) {
         char *second_cmd[3] = {"KEYS", "ENTRIES", "SNAPSHOT"};
-        isFound = false;
-        i = 0;
-        int index_two = 0;
-        while (i < sizeof(second_cmd)) {
-            if (strcasecmp(cmd[1], second_cmd[i]) == true) {
-                isFound = true;
+        while (i < (sizeof(second_cmd) / sizeof(commands[0]))) {
+            int result = strcasecmp(cmd[0], commands[i]);
+            if (result == 0) {
+                isFound2 = true;
                 index_two = i;
                 break;
             }
             i++;
         }
-        if (isFound == false) {
+        if (isFound2 == false) {
             return;
             //incorrect command
         }
+
     }
 
-    if(index_one==0){
+    if (index_one == 0) {
         free(current_version);
         command_bye();
-    } else if(index_one==1){
+        exit(0);
+    } else if (index_one == 1) {
         command_help();
-    } else if(index_one==2){
+    } else if (index_one == 2) {
+        command_list(current_version, cmd_counter, *cmd, index_two);
+    } else if (index_one == 3) {
 
-    } else if(index_one==3){
+    } else if (index_one == 4) {
 
-    } else if(index_one==4){
+    } else if (index_one == 5) {
 
-    } else if(index_one==5){
+    } else if (index_one == 6) {
 
-    } else if(index_one==6){
+    } else if (index_one == 7) {
 
-    } else if(index_one==7){
+    } else if (index_one == 8) {
 
-    } else if(index_one==8){
+    } else if (index_one == 9) {
 
-    } else if(index_one==9){
+    } else if (index_one == 10) {
 
-    } else if(index_one==10){
+    } else if (index_one == 11) {
 
-    } else if(index_one==11){
+    } else if (index_one == 12) {
 
-    } else if(index_one==12){
+    } else if (index_one == 13) {
 
-    } else if(index_one==13){
+    } else if (index_one == 14) {
 
-    } else if(index_one==14){
+    } else if (index_one == 15) {
 
-    } else if(index_one==15){
+    } else if (index_one == 16) {
 
-    } else if(index_one==16){
+    } else if (index_one == 17) {
 
-    } else if(index_one==17){
+    } else if (index_one == 18) {
 
-    } else if(index_one==18){
+    } else if (index_one == 19) {
 
-    } else if(index_one==19){
+    } else if (index_one == 20) {
 
-    } else if(index_one==20){
+    } else if (index_one == 21) {
 
-    } else if(index_one==21){
+    } else if (index_one == 22) {
 
-    } else if(index_one==22){
+    } else if (index_one == 23) {
 
-    } else if(index_one==23){
+    } else if (index_one == 24) {
 
-    } else if(index_one==24){
-
-    } else if(index_one==25){
-
-    } else if(index_one==26){
-
-    } else if(index_one==27){
-
-    } else if(index_one==28){
+    } else if (index_one == 25) {
 
     } else {
         return;
@@ -270,32 +271,33 @@ void command_compare(snapshot *current_version, int cmdcounter, char *cmd) {
 }
 
 int main(void) {
-
+    /*
+     * Main function that handles the user input by breaking the string up into its components.
+     */
     char line[MAX_LINE];
-    entry *current_version = malloc(sizeof(entry));
+    snapshot *current_version = malloc(sizeof(snapshot));
+    current_version->entries = NULL;
     current_version->next = NULL;
     current_version->prev = NULL;
 
     while (true) {
         printf("> ");
-        fgets(line, sizeof(line), stdin);
         if (fgets(line, MAX_LINE, stdin) == NULL) {
             command_bye();
             return 0;
         }
-        strtok(line, "\n");
 
         char *cmd[MAX_COMMAND];
         int i = 0;
+        strtok(line, "\n");
         cmd[i] = strtok(line, " ");
 
         while (cmd[i] != NULL) {
             i++;
-            cmd[i] = strtok(line, " ");
+            cmd[i] = strtok(NULL, " ");
         }
-        int cmdcounter = i;
-        command_compare(current_version, cmdcounter, *cmd);
-
+        int cmd_counter = i;
+        command_compare(current_version, cmd_counter, cmd);
     }
     free(current_version);
     return 0;
